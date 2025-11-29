56bbde91ff037921bc465168123a2a29e14c129b
QingFeng-awa <qingfeng_awa@163.com>
2025-11-29 16:51:34 +0800

    补全缺失引用

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from astrbot.core.message.message_event_result import MessageChain
 
 from .Variable.sqlTable import VERIFY_LOG, VERIFY_USER_DATA, VERIFY_GROUP_LOG, REQUEID_TABLES
 from .Variable.messageTemplate import help_menu, new_user_join
+from api import database_manage
 
 class LinkAmongUs(Star):
     def __init__(self, context: Context, config: AstrBotConfig): # AstrBotConfig 继承自 Dict，拥有字典的所有方法。
