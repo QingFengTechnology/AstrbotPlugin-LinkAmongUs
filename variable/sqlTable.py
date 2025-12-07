d022b849653918b2981928f2c78d7ca962e5b60a
QingFeng-awa <qingfeng_awa@163.com>
2025-12-07 16:06:14 +0800

    修复依赖引用异常问题
    `git config core.ignorecase false`

---
+++ variable/sqlTable.py
@@ -0,0 +1,44 @@
+VERIFY_LOG = """
+CREATE TABLE IF NOT EXISTS VerifyLog (
+SQLID smallint NOT NULL AUTO_INCREMENT,
+CreateTime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+Status set('Created','Retrying','Verified','Cancelled','Expired') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+UserQQID varchar(13) NOT NULL,
+UserFriendCode varchar(32) NOT NULL,
+VerifyCode varchar(7) NOT NULL,
+PRIMARY KEY (SQLID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+"""
+
+VERIFY_USER_DATA = """
+CREATE TABLE IF NOT EXISTS VerifyUserData (
+QLID smallint NOT NULL AUTO_INCREMENT,
+LastUpdated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+UserQQName varchar(40) DEFAULT NULL,
+UserQQID varchar(13) NOT NULL,
+UserAmongUsName varchar(11) DEFAULT NULL,
+UserFriendCode varchar(32) NOT NULL,
+UserPuid varchar(48) NOT NULL,
+UserHashedPuid varchar(11) NOT NULL,
+UserUdpPlatform varchar(32) NOT NULL,
+UserTokenPlatform varchar(32) NOT NULL,
+UserUdpIP varchar(32) NOT NULL,
+UserHttpIP varchar(128) NOT NULL,
+PRIMARY KEY (SQLID),
+UNIQUE KEY unique_user_data (UserQQID, UserFriendCode, UserPuid, UserHashedPuid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+"""
+
+VERIFY_GROUP_LOG = """
+CREATE TABLE IF NOT EXISTS VerifyGroupLog (
+SQLID smallint NOT NULL AUTO_INCREMENT,
+CreateTime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+Status set('Created','Banned','Unbanned','Kicked','Cancelled') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+VerifyUserID varchar(13) NOT NULL,
+BanGroupID varchar(13) NOT NULL,
+KickTime datetime NOT NULL,
+PRIMARY KEY (SQLID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+"""
+
+REQUEID_TABLES = ["VerifyUserData", "VerifyLog", "VerifyGroupLog"]
